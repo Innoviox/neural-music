81a52708d093477b3d946d65d6bae3b3b4fa81fc
Innoviox <sarras305@gmail.com>
2018-02-10 17:11:46 -0500

    made note-pair folder & song!

---
+++ train-gen/coerce.py
@@ -0,0 +1,70 @@
+import pickle
+import numpy as np
+import pandas as pd
+import midi
+import random
+
+hm_lines = 100000
+
+def read_notes(fn):
+    print("Reading:", fn)
+    notes = []
+    f = midi.read_midifile(fn)
+    for track in f:
+        for note in track:
+            if isinstance(note, midi.NoteOnEvent):
+                notes.append(note.data)
+    print("Read:", len(notes), "notes")
+    return notes
+
+# read_notes('samples/bach/bach_846.mid')
+
+def sample_handling(files): #, notes, classification):
+    def _sample_handling(sample):
+        featureset = []
+        notes = read_notes(sample)
+        for note1, note2 in zip(notes, notes[1:]):
+            featureset.append([note1, note2])
+        return featureset
+    ret = [f for file in files for f in _sample_handling(file)]
+    return ret
+
+def create_feature_sets_and_labels(files, test_size = 0.1):
+    _features = np.array(sample_handling(files))
+    testing_size = int(test_size*len(_features))
+    #print(_features)
+    classes = _features[:,0][:-testing_size]
+    uns = np.unique(classes)
+    n_classes = len(uns)
+    features = []
+    for f in _features:
+        result = np.zeros(n_classes)
+        #print(classes, f[1], classes==f[1], np.where(classes==f[1])[0])
+        #print(list(classes).index(f[1]))
+        result[np.where(uns==f[1][0])] = 1
+        #print(np.where(uns==f[1][0]))
+        #input(result)#, np.where(classes==f[1])[0], list(classes).index(f[1]))
+        features.append([f[0], result])
+    
+    random.shuffle(features)
+    features = np.array(features)
+
+    testing_size = int(test_size*len(features))
+
+    train_x = list(features[:,0][:-testing_size])
+    train_y = list(features[:,1][:-testing_size])
+    test_x = list(features[:,0][-testing_size:])
+    test_y = list(features[:,1][-testing_size:])
+    return train_x, train_y, test_x, test_y, n_classes
+
+if __name__ == '__main__':
+    import os
+    files = []
+    for root, dirs, fs in os.walk('../samples'):
+        if not dirs and 'bach' in root:
+            for file in fs:
+                files.append(os.path.join(root, file))
+    train_x, train_y, test_x, test_y, n_classes = create_feature_sets_and_labels(files)
+    # if you want to pickle this data:
+    with open('note_features.pickle','wb') as f:
+        pickle.dump([train_x, train_y, test_x, test_y, n_classes],f)
---
+++ train-gen/network.py
@@ -0,0 +1,89 @@
+import tensorflow as tf
+import pickle
+import numpy as np
+train_x, train_y, test_x, test_y, n_classes = pickle.load(open('note_features.pickle', 'rb'))
+
+n_nodes_hl1 = 500
+n_nodes_hl2 = 500
+
+#n_classes = 2
+hm_data = 2000000
+
+batch_size = 100
+hm_epochs = 10
+
+n = 2
+x = tf.placeholder('float', [None, n])
+y = tf.placeholder('float')
+
+current_epoch = tf.Variable(1)
+
+hidden_1_layer = {'f_fum':n_nodes_hl1,
+                  'weight':tf.Variable(tf.random_normal([n, n_nodes_hl1])),
+                  'bias':tf.Variable(tf.random_normal([n_nodes_hl1]))}
+
+hidden_2_layer = {'f_fum':n_nodes_hl2,
+                  'weight':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+                  'bias':tf.Variable(tf.random_normal([n_nodes_hl2]))}
+
+output_layer = {'f_fum':None,
+                'weight':tf.Variable(tf.random_normal([n_nodes_hl2, n_classes])),
+                'bias':tf.Variable(tf.random_normal([n_classes]))}
+
+def neural_network_model(data):
+    l1 = tf.add(tf.matmul(data,hidden_1_layer['weight']), hidden_1_layer['bias'])
+    l1 = tf.nn.relu(l1)
+
+    l2 = tf.add(tf.matmul(l1,hidden_2_layer['weight']), hidden_2_layer['bias'])
+    l2 = tf.nn.relu(l2)
+
+    output = tf.matmul(l2,output_layer['weight']) + output_layer['bias']
+    return output
+
+def train_neural_network(x):
+    prediction = neural_network_model(x)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=prediction, labels=y))
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+    hm_epochs = 10
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for epoch in range(hm_epochs):
+            epoch_loss = 0
+            i = 1
+            while i < len(train_x):
+                start = i
+                end = i+batch_size
+                batch_x = np.array(train_x[start:end])
+                batch_y = np.array(train_y[start:end])
+                _, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
+                epoch_loss += c
+                i+=batch_size                  
+            print('Epoch', epoch+1, 'completed out of', hm_epochs, 'loss:', epoch_loss)
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        print('Accuracy:', accuracy.eval({x:test_x,y:test_y}))
+        save = saver.save(sess, "./model.ckpt")
+        print("Model saved to file:", save)
+
+#train_neural_network(x)
+
+def use_neural_network(input_data):
+    prediction = neural_network_model(x)
+    # with open('lexicon.pickle','rb') as f:
+    #     lexicon = pickle.load(f)
+        
+    with tf.Session() as sess:
+        saver = tf.train.Saver()
+        sess.run(tf.global_variables_initializer())
+        saver.restore(sess,"model.ckpt")
+        features = input_data[:]
+        i = 0
+        while 1:
+            result = (sess.run(tf.argmax(prediction.eval(feed_dict={x:[features]}),1)))
+            print(result)
+            features = [result[0], i]
+            i += 1
+
+use_neural_network([48, 0])
